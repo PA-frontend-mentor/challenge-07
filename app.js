24cb3826231f029e960c626f877a90c97d37c68f
ParsaArvanehPA <parsa.arvaneh@gmail.com>
2021-05-11 21:03:45 +0430

    [ADD] btn click + hover,active style fixed in desktop and phone view

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+const slideImg = document.querySelector(".writer-img");
+const leftBtn = document.querySelector(".left-btn");
+const rightBtn = document.querySelector(".right-btn");
+const quote = document.querySelector(".quote");
+const witerName = document.querySelector(".name");
+const writerTitle = document.querySelector(".job-title");
+
+var index = 0;
+var len;
+
 fetch("info.json")
   .then((response) => response.json())
   .then((data) => {
@@ -5,5 +15,25 @@ fetch("info.json")
   });
 
 const main = ({ data }) => {
-  console.log(data);
+  len = data.length;
+
+  leftBtn.addEventListener("click", () => clickHandler("left"));
+  rightBtn.addEventListener("click", () => clickHandler("right"));
+
+  const clickHandler = (dir) => {
+    if (dir == "right") {
+      index++;
+      if (index == len) index = 0;
+    } else {
+      index--;
+      if (index < 0) index = len - 1;
+    }
+
+    quote.innerText = data[index].quote;
+    witerName.innerText = data[index].name;
+    writerTitle.innerText = data[index].job;
+
+    slideImg.style.background = `url(${data[index].imgSrc}) no-repeat`;
+    slideImg.style.backgroundSize = "cover";
+  };
 };
